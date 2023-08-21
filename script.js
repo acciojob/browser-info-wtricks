d622feeacf1465445176bed205849b430ce5d8f0
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-21 11:22:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const element = document.getElementById("browser-info");
 const browserName = navigator.userAgent;
 const browserVersion = (navigator.appVersion).match(/.*?Version\/(.*?)(?= |$)/);
 
-element.textContent = `You are using ${browserName} ${(browserVersion ? browserVersion[1] : "")}`
+element.textContent = `You are using ${browserName} ${(browserVersion ? browserVersion[1] : "version")}`
